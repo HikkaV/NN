fcaf40c424c24cc3fd382f1e7d53164ed3d37187
HikkaV <urumipainblackreaper@gmail.com>
2019-01-23 14:47:04 +0200

    NN

---
+++ venv/Main.py
@@ -0,0 +1,6 @@
+import NN
+from NN import NN
+if __name__ == '__main__':
+    nn = NN()
+    model = nn.load_model()
+    nn.predict(model=model, path = '/home/hikkav/environments/my_env/train/car23374.jpg')
---
+++ venv/Settings.py
@@ -0,0 +1,4 @@
+path_to_model = '/home/hikkav/environments/my_env/CNNmodel.h5'
+adress = 'train.hdf5'  # address to where you want to save the hdf5 file while preprocessing the data
+path_to_car = '/home/hikkav/environments/my_env/mytraindata/*.jpg'  # path where the one saved dataset
+
